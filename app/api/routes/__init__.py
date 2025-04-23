0d98b21070841d17fd0220d53e7f4df6fabd8cde
Sadoklaoo <sadok.laouissi.sl@gmail.com>
2025-04-23 14:53:25 +0200

    feat: add dashboard route to fetch user-specific device and sensor data with error handling

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from app.api.routes.user_routes import router as user_router
 from app.api.routes.devices_routes import router as device_router
 from app.api.routes.sensor_routes import router as sensor_router
+from app.api.routes.dashboard_routes import router as dashboard_router
 
 api_router = APIRouter()
 
 api_router.include_router(user_router, prefix="/users", tags=["Users"])
 api_router.include_router(device_router, prefix="/devices", tags=["Devices"])
 api_router.include_router(sensor_router, prefix="/sensors", tags=["Sensors"])
+api_router.include_router(dashboard_router, prefix="/dashboard", tags=["Dashboard"])
